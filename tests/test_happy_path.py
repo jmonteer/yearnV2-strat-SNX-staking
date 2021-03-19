912b3ac97433f2fc981ffee110ad31b6591a466e
jmonteer <jmonteer@protonmail.com>
2021-03-19 22:02:51 +0100

    fix: temp commit

--- tests/test_happy_path.py
+++ tests/test_happy_path.py
@@ -3,7 +3,6 @@ from brownie import Wei, Contract
 
 
 def test_happy_path(gov, vault, strategy, snx, susd, snx_whale, bob):
-
     snx.transfer(bob, Wei("1000 ether"), {"from": snx_whale})
     snx.approve(vault, 2 ** 256 - 1, {"from": bob})
     vault.deposit({"from": bob})
