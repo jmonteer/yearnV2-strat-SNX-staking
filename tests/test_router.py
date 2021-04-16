b0c53b147d24bdadc8592123812b56c02993b21b
jmonteer <jmonteer@protonmail.com>
2021-04-16 11:59:12 +0200

    fix: test passing

--- tests/test_router.py
+++ tests/test_router.py
@@ -8,15 +8,15 @@ def test_router(vault, strategy, susd, susd_whale):
 
     # Shouldn't be able to set the router to a random address
     with brownie.reverts():
-        strategy.setRouter(susd, {"from": strategy.strategist()})
+        strategy.setRouter(5, {"from": strategy.strategist()})
 
-    strategy.setRouter(strategy.uniswap(), {"from": strategy.strategist()})
+    strategy.setRouter(0, {"from": strategy.strategist()})
     susd.transfer(strategy, Wei("1000 ether"), {"from": susd_whale})
     strategy.harvest({"from": strategy.strategist()})
     gain = vault.strategies(strategy).dict()["totalGain"]
     assert gain > 0
 
-    strategy.setRouter(strategy.sushiswap(), {"from": strategy.strategist()})
+    strategy.setRouter(1, {"from": strategy.strategist()})
     susd.transfer(strategy, Wei("2000 ether"), {"from": susd_whale})
     strategy.harvest({"from": strategy.strategist()})
     new_gain = vault.strategies(strategy).dict()["totalGain"]
